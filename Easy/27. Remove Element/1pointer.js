e66e4ddf9d1a7fcf99403f6fbc1007ad581b4dc9
Steve <jim97731@gmail.com>
2023-03-19 22:29:19 +0800

    1 pointer solution

---
+++ Easy/27. Remove Element/1pointer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let pointer = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[pointer] = nums[i]
+            pointer ++
+        } else nums[i] = null
+    }
+
+    return pointer
+};

035f07ae6cc3c2f957548347365862ac5dc6d98c
Steve <jim97731@gmail.com>
2023-04-09 23:06:07 +0800

    Solution Implemented

--- Easy/202. Happy Number/index.js
+++ Easy/202. Happy Number/index.js
@@ -1,1 +1,19 @@
 
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    let seen = {}
+    while (n !== 1 && !seen[n]) {
+        seen[n] = true
+        n = ('' + n).split('')
+        
+        let result = 0
+        n.forEach(num => result += Math.pow(Number(num), 2))
+
+        n = result
+    }
+
+    return n === 1
+};

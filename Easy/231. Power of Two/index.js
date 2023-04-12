ca9cf041a9ac7d42ce63b56871b73870e082bbc5
Steve <jim97731@gmail.com>
2023-04-12 21:43:46 +0800

    Semi Binary Search Algo Implemented

--- Easy/231. Power of Two/index.js
+++ Easy/231. Power of Two/index.js
@@ -1,1 +1,23 @@
 
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    let slow = 0
+    let fast = 2
+
+    while (slow < fast) {
+        let slowResult = Math.pow(2, slow)
+        let fastResult = Math.pow(2, fast)
+
+        if (slowResult === n || fastResult === n) return true
+        else {
+            if (slowResult < n && fastResult < n) fast = fast * 2
+            else if (slowResult < n && fastResult > n) fast--
+            slow++
+        }
+    }
+
+    return Math.pow(2, slow) === n
+};

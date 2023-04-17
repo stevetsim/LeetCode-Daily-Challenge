d85c9dbc1a456cec2ea42fa57b8e3004ffebe7d9
Steve <jim97731@gmail.com>
2023-04-17 22:15:02 +0800

    Solution 1 Implemented

--- Easy/268. Missing Number/solution1.js
+++ Easy/268. Missing Number/solution1.js
@@ -1,1 +1,13 @@
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length
+    let total = n * (n + 1) / 2
+
+    let sum = nums.reduce((accu, curr) => accu + curr, 0)
+
+    return total - sum
+};

8c07b7c1122f19c27a98ab0efed3f458e2c35d77
Steve <jim97731@gmail.com>
2023-04-23 23:20:36 +0800

    Pointer solution implemented

--- Easy/344. Reverse String/index.js
+++ Easy/344. Reverse String/index.js
@@ -1,1 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let left = 0
+    let right = s.length - 1
 
+    while (left < right) {
+        let temp = s[right]
+        s[right] = s[left]
+        s[left] = temp
+
+        left ++
+        right --
+    }
+
+    return s
+};

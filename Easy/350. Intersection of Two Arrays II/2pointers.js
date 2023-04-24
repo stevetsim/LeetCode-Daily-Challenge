1be475b424602461063c4e1807d21b7b5506726d
Steve <jim97731@gmail.com>
2023-04-24 23:53:16 +0800

    two pointer solution implemented

---
+++ Easy/350. Intersection of Two Arrays II/2pointers.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    nums1.sort((a,b) => a - b)
+    nums2.sort((a,b) => a - b)
+    let p1 = 0
+    let p2 = 0
+    let result = []
+
+    while (p1 < nums1.length && p2 < nums2.length) {
+        if (nums1[p1] === nums2[p2]) {
+            result.push(nums1[p1])
+            p1++
+            p2++
+        } else if (nums1[p1] < nums2[p2]) p1++
+        else if (nums1[p1] > nums2[p2]) p2++
+    }
+
+    return result
+};
